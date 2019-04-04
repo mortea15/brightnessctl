d9140bcb97b1728726928d16e1fb62ad37015159
mortea15 <me@mortenamundsen.me>
2019-04-04 21:25:06 +0200

    changed device, stepsize to match keyboard leds

--- configuration.sh
+++ configuration.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-brightFolder="/sys/class/backlight/intel_backlight/"
-stepSize=25
+brightFolder="/sys/class/leds/asus::kbd_backlight/"
+stepSize=1
 currentbrigth=`cat $brightFolder"brightness"`
